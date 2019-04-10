ff50cd9186cc45c5f60d872ae797d0d722abea30
maha276 <mahaforo276@gmail.com>
2019-04-10 12:49:27 +0300

    finished the tests and logic files

--- test.js
+++ test.js
@@ -25,6 +25,14 @@ test('Testing tape', function(t) {
   t.end();
 });
 
+test('leaves the original array unchanged', function(t){
+  var actual = testTodos === logic.addTodo(testTodos, newTodoTest);
+  var expected = false;
+
+  t.equal(actual, expected, 'Return array is a copy of original array');
+  t.end();
+});
+
 test('Testing addTodo', function(t){
 var actual = logic.addTodo(testTodos, newTodoTest);
 var expected = [
@@ -49,11 +57,53 @@ t.deepEqual(actual, expected, 'New todo has been added to the todo array');
 t.end();
 });
 
-test('leaves the original array unchanged', function(t){
-  var actual = testTodos === logic.addTodo(testTodos, newTodoTest);
+var testIdToDelete = 1;
+
+test('leaves the input argument unchanged', function(t){
+  var actual = testTodos === logic.deleteTodo(testTodos, testIdToDelete);
   var expected = false;
 
   t.equal(actual, expected, 'Return array is a copy of original array');
-
   t.end();
+});
+
+test('Testing deleteTodo', function(t){
+  var actual = logic.deleteTodo(testTodos, testIdToDelete);
+  var expected = [
+    {
+     id: 2,
+     description: "Do my homework",
+     done: false
+    }
+  ];
+
+  t.deepEqual(actual, expected, 'choosen todo with id has been deleted from the list');
+  t.end();
+});
+
+var testIdToMark = 2;
+test('leaves the input argument unchanged', function(t){
+  var actual = testTodos === logic.markTodo(testTodos, testIdToMark);
+  var expected = false;
+
+  t.equal(actual, expected, 'choosen todo with id has been marked in the list');
+  t.end();
+});
+
+test('Testing markTodo', function(t) {
+  var actual = logic.markTodo(testTodos, testIdToMark);
+  var expected = [
+    {
+     id:1,
+     description: "finish todo app",
+     done: false
+   },
+   {
+    id: 2,
+    description: "Do my homework",
+    done: true
+   }
+ ];
+ t.deepEqual(actual, expected, 'choosen todo with id has been marked in the list');
+ t.end();
 });
